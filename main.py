61780759f725eac2f13cd6a62a218a26d7103d10
Derek Stiles <derek.r.stiles@gmail.com>
2024-02-18 21:26:48 -0500

    wtf

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+#!/usr/bin/python3
 # data.db file from Delver Lens N extracted apk.
 import os
 import sys
-
-from PySide2 import QtWidgets, QtGui
-
 import json
+import time
+import queue
 import sqlite3
+import asyncio
+import zipfile
+import requests
+import tempfile
+
 from datetime import datetime
 from functools import lru_cache
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
+from PySide2 import QtWidgets
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        # Class vars
+        self.running = False
+        self.apkdatabase = None
+        self.apkc = None
+        self.offlinescryfall = None
+        self.dlens = None
+        self.dlensc = None
+
+        # GUI vars
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.setFixedWidth(800)
@@ -93,32 +108,52 @@ class Ui_MainWindow(object):
         self.menuMenu.addAction(self.actionAbout)
         self.menuMenu.addAction(self.actionQuit)
 
-
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
 
-        for file in os.listdir(os.getcwd()):
+        for file in os.listdir():
             if file.endswith(".db"):
-                self.lineEdit.setText(os.path.join(os.getcwd(), file))
+                self.lineEdit.setText(file)
             elif file.endswith(".json"):
-                self.lineEdit_2.setText(os.path.join(os.getcwd(), file))
+                self.lineEdit_2.setText(file)
             elif file.endswith(".dlens"):
-                self.lineEdit_3.setText(os.path.join(os.getcwd(), file))
+                self.lineEdit_3.setText(file)
+
+        # If no db was found, fecth the latest
+        # TODO update calls to async to not block GUI from opening while files are being fetched
+        # TODO add button to fetch the latest files
+        work_queue = queue.Queue()
+        if not self.lineEdit.text():
+            self.pushButton.setEnabled(False)
+            self.lineEdit.setText('Fetching...')
+            self.lineEdit.setText(getApkDatabase())
+            self.pushButton.setEnabled(True)
+
+        # If no json was found, fecth the latest
+        if not self.lineEdit_2.text():
+            self.pushButton_2.setEnabled(False)
+            self.lineEdit_2.setText('Fetching...')
+            self.lineEdit_2.setText(getScryfallJson())
+            self.pushButton_2.setEnabled(True)
+
+        # If no dlens was found, fecth the latest
+        if not self.lineEdit_3.text():
+            self.pushButton_3.setEnabled(False)
+            self.lineEdit_3.setText('Fetching...')
+            self.lineEdit_3.setText(getDlensBackup())
+            self.pushButton_3.setEnabled(True)
 
 
     def setRunning(self, bool):
-        global running
-        running = bool
+        self.running = bool
         if bool == False:
             self.access_file.cache_clear()
             self.getcarddatabyid.cache_clear()
 
 
     def getRunning(self):
-        global running
-        return running
-
+        return self.running
 
     def showAbout(self):
         self.textEdit.append(f"dlensExporter Version 1.0. \nWritten by jertzukka@github under MIT License.")
@@ -161,22 +196,18 @@ class Ui_MainWindow(object):
 
 
     def connectapkdatabase(self):
-        global apkdatabase
-        apkconn = sqlite3.connect(apkdatabase)
-        global apkc
-        apkc = apkconn.cursor()
-
+        apkconn = sqlite3.connect(self.apkdatabase)
+        self.apkc = apkconn.cursor()
 
     def connectdlensdatabase(self):
-        dlensconn = sqlite3.connect(dlens)
-        global dlensc
-        dlensc = dlensconn.cursor()
-
+        print (self.dlens)
+        dlensconn = sqlite3.connect(self.dlens)
+        self.dlensc = dlensconn.cursor()
 
     @lru_cache(maxsize=1)
     def access_file(self):
         try:
-            with open(offlinescryfall, 'r', encoding='utf-8') as json_data:
+            with open(self.offlinescryfall, 'r', encoding='utf-8') as json_data:
                 json_data = json.load(json_data)
                 return json_data
         except MemoryError:
@@ -192,8 +223,8 @@ class Ui_MainWindow(object):
     @lru_cache(maxsize=128)
     def getcarddatabyid(self, id):
         t = (id,)
-        apkc.execute('SELECT scryfall_id FROM cards WHERE _id=?', t)
-        scryfall_id = apkc.fetchone()[0]
+        self.apkc.execute('SELECT scryfall_id FROM cards WHERE _id=?', t)
+        scryfall_id = self.apkc.fetchone()[0]
 
         try:
             for each in self.access_file():
@@ -204,88 +235,142 @@ class Ui_MainWindow(object):
 
 
     def startexport(self):
-        global apkdatabase, offlinescryfall, dlens
         self.setRunning(True)
-        apkdatabase = self.lineEdit.text()
-        offlinescryfall = self.lineEdit_2.text()
-        dlens = self.lineEdit_3.text()
+        self.apkdatabase = self.lineEdit.text()
+        self.offlinescryfall = self.lineEdit_2.text()
+        self.dlens = self.lineEdit_3.text()
 
         # Open both SQLite files
         self.connectapkdatabase()
         self.connectdlensdatabase()
 
-        # Get all cards from .dlens file
-        dlensc.execute('SELECT * from cards')
-        cardstoimport = dlensc.fetchall()
+
+        # Set new collectioon dir
+        now = datetime.now()
+        collectionDir = 'Collection-%s' % now.strftime("%d_%m_%Y-%H_%M_%S")
+        if not os.path.exists(collectionDir):
+            os.makedirs(collectionDir)
+
+        fileType = 'csv'
+        topLine = f'Count,Tradelist Count,Name,Edition,Card Number,Condition,Language,Foil,Signed,Artist Proof,Altered Art,Misprint,Promo,Textless,My Price\n'
+        listTypes = {
+            1: 'list',
+            2: 'deck',
+            3: 'want',
+            4: 'trade'
+        }
+
+        # Create sub dir for list
+        self.dlensc.execute('SELECT * from lists')
+        listsToImport = self.dlensc.fetchall()
+        lists = {}
+
+        for iteration, each  in enumerate(listsToImport):
+            print(each[0])
+            print(listsToImport[iteration])
+            #TODO DEREK make file name always valild. `/` and ` ` should be replaced with `-`
+            invalid = '<>:"/\|?* '
+            collectionName = each[3]
+            for char in invalid:
+                collectionName = collectionName.replace(char, '-')
+
+            lists[each[0]] = {
+                'listType': each[2],
+                'name': collectionName,
+                'file': '%s/%s-%s.%s' % (collectionDir, listTypes[each[2]], collectionName, fileType),
+                'groupFile': '%s/%ss.%s' % (collectionDir, listTypes[each[2]], fileType),
+            }
+            print("here")
+            print(lists[each[0]])
+            print(lists[each[0]]['file'])
+            with open(lists[each[0]]['file'], "w+", encoding="utf-8") as file:
+                file.write(topLine)
+            with open(lists[each[0]]['groupFile'], "w+", encoding="utf-8") as file:
+                file.write(topLine)
 
         # Set new .csv file name
         now = datetime.now()
         newcsvname = now.strftime("%d_%m_%Y-%H_%M_%S") + ".csv"
 
+
+        # Get all cards from .dlens file
+        self.dlensc.execute('SELECT * from cards')
+        cardstoimport = self.dlensc.fetchall()
+        total = len(cardstoimport)
+        errors = 0
         # For each card, match the id to the apk database and with scryfall_id search further data from Scryfall database.
-        with open(newcsvname, "a", encoding="utf-8") as file:
-            total = len(cardstoimport)
-            errors = 0
-            file.write(f'Count,Tradelist Count,Name,Edition,Card Number,Condition,Language,Foil,Signed,Artist Proof,Altered Art,Misprint,Promo,Textless,My Price\n')
-            for iteration, each in enumerate(cardstoimport):
-                if iteration == 0:
-                    self.textEdit.append(f"Preparing files, this might take a bit...")
-                    QtWidgets.QApplication.processEvents()
-                    print("Preparing files, this might take a bit...")
-                    self.access_file()
-                if not self.getRunning():
-                    break
-                id = each[1]
-                foil = each[2]
-                quantity = each[4]
-                carddata = self.getcarddatabyid(id)
-                self.progressBar.setValue((iteration + 1)/total*100)
-                self.textEdit.append(f"[ {iteration + 1} / {total} ] Getting data for ID: {id}")
+        for iteration, each in enumerate(cardstoimport):
+            if iteration == 0:
+                self.textEdit.append(f"Preparing files, this might take a bit...")
                 QtWidgets.QApplication.processEvents()
+                print("Preparing files, this might take a bit...")
+                self.access_file()
+            if not self.getRunning():
+                break
 
-                if carddata is None:
-                    self.textEdit.append(f"[ {iteration + 1} / {total} ] Card could not be found from the Scryfall .json with ID: {id}")
-                    print("[", iteration + 1, "/", total, "] Card could not be found from the Scryfall .json with ID:", id)
-                    QtWidgets.QApplication.processEvents()
-                    errors = errors + 1
-                    continue
+            id = each[1]
+            foil = each[2]
+            quantity = each[4]
+            list = each[7]
+            condition = each[9]
+            language = each[10]
 
-                number = carddata['collector_number']
-                language = each[10]
+            if list < 0:
+                print("idk why < 0")
+                continue
 
-                # Fix names from Scryfall to Deckbox
-                name = carddata['name']
-                if name == "Solitary Hunter // One of the Pack":
-                    name = "Solitary Hunter"
+            self.progressBar.setValue((iteration + 1) / total * 100)
+            self.textEdit.append(f"[ {iteration + 1} / {total} ] Getting data for ID: {id}")
+            QtWidgets.QApplication.processEvents()
 
-                # Fix condition names from Scryfall to Deckbox
-                condition = each[9]
-                if condition == "Moderately Played":
-                    condition = "Played"
-                elif condition == "Slighty Played":
-                    condition = "Good (Lightly Played)"
+            carddata = self.getcarddatabyid(id)
+            if carddata is None:
+                self.textEdit.append(f"[ {iteration + 1} / {total} ] Card could not be found from the Scryfall .json with ID: {id}")
+                print("[", iteration + 1, "/", total, "] Card could not be found from the Scryfall .json with ID:", id)
+                QtWidgets.QApplication.processEvents()
+                errors = errors + 1
+                continue
 
-                # Fix set names from Scryfall to Deckbox
-                set = carddata['set_name']
-                if set == "Magic 2015":
-                    set = "Magic 2015 Core Set"
-                elif set == "Magic 2014":
-                    set = "Magic 2014 Core Set"
-                elif set == "Modern Masters 2015":
-                    set = "Modern Masters 2015 Edition"
-                elif set == "Modern Masters 2017":
-                    set = "Modern Masters 2017 Edition"
-                elif set == "Time Spiral Timeshifted":
-                    set = 'Time Spiral ""Timeshifted""'
-                elif set == "Commander 2011":
-                    set = "Commander"
-                elif set == "Friday Night Magic 2009":
-                    set = "Friday Night Magic"
-                elif set == "DCI Promos":
-                    set = "WPN/Gateway"
+            number = carddata['collector_number']
 
-                file.write(
-                    f'''"{quantity}","{quantity}","{name}","{set}","{number}","{condition}","{language}","{foil}","","","","","","",""\n''')
+            # Fix names from Scryfall to Deckbox
+            name = carddata['name']
+            if name == "Solitary Hunter // One of the Pack":
+                name = "Solitary Hunter"
+
+            # Fix set names from Scryfall to Deckbox
+            set = carddata['set_name']
+            if set == "Magic 2015":
+                set = "Magic 2015 Core Set"
+            elif set == "Magic 2014":
+                set = "Magic 2014 Core Set"
+            elif set == "Modern Masters 2015":
+                set = "Modern Masters 2015 Edition"
+            elif set == "Modern Masters 2017":
+                set = "Modern Masters 2017 Edition"
+            elif set == "Time Spiral Timeshifted":
+                set = 'Time Spiral ""Timeshifted""'
+            elif set == "Commander 2011":
+                set = "Commander"
+            elif set == "Friday Night Magic 2009":
+                set = "Friday Night Magic"
+            elif set == "DCI Promos":
+                set = "WPN/Gateway"
+
+            # Fix condition names from Scryfall to Deckbox
+            if condition == "Moderately Played":
+                condition = "Played"
+            elif condition == "Slighty Played":
+                condition = "Good (Lightly Played)"
+
+            writeData = f'''"{quantity}","{quantity}","{name}","{set}","{number}","{condition}","{language}","{foil}","","","","","","",""\n'''
+            print(list)
+            print(lists[list]['file'])
+            print(lists[list]['groupFile'])
+            with open(lists[list]['file'], "a", encoding="utf-8") as file:
+                file.write(writeData)
+            with open(lists[list]['groupFile'], "a", encoding="utf-8") as file:
+                file.write(writeData)
 
         if self.getRunning():
             if errors > 0:
@@ -300,15 +385,62 @@ class Ui_MainWindow(object):
             print(f"Stopping early, imported {iteration - errors} out of {total} cards in {newcsvname}")
             self.textEdit.append(f"Stopping early, imported {iteration - errors} out of {total} entries in {newcsvname}")
 
-
-
         QtWidgets.QApplication.processEvents()
-
         self.setRunning(False)
 
 
-if __name__ == '__main__':
+def getApkDatabase():
+    url = 'https://delver-public.s3.us-west-1.amazonaws.com/app-release.apk'
+    filename = 'app-release.apk'
+    cwd = os.getcwd()
 
+    with tempfile.TemporaryDirectory() as tmpDir:
+        fullPath = f'{tmpDir}/{filename}'
+        with requests.get(url, stream=True) as r:
+            open(fullPath, 'wb').write(r.content)
+        with zipfile.ZipFile(fullPath, 'r') as zip_ref:
+            zip_ref.extractall(tmpDir)
+            os.rename(f'{tmpDir}/res/Cc.db', f'{cwd}/Cc.db')
+            return 'Cc.db'
+
+    return ''
+
+
+def getScryfallJson():
+    try:
+        url = 'https://api.scryfall.com/bulk-data'
+        response = requests.get(url)
+        bulkData = json.loads(response.text)
+        for data in bulkData['data']:
+            if data['name'] == "Default Cards":
+                url = data['download_uri']
+                fileName = url.split('/')[-1]
+                if fileName in os.listdir():
+                    return fileName
+                with requests.get(url, stream=True) as r:
+                    open(fileName, 'wb').write(r.content)
+
+                for file in os.listdir():
+                    if file.endswith('.json') and file != fileName:
+                        os.remove(file)
+                return fileName
+    except:
+        pass
+    return ''
+
+
+def getDlensBackup():
+    with open('dropbox.link', 'r') as file:
+        url = file.read().rstrip()
+        data = file.read().replace('\n', '')
+        fileName = f'user-{time.strftime("%Y%m%d")}.dlens'
+        with requests.get(url, stream=True) as r:
+            open(fileName, 'wb').write(r.content)
+            return fileName
+    return ''
+
+
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     ex = Ui_MainWindow()
     w = QtWidgets.QMainWindow()
